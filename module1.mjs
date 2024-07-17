12009ee3baa3558593c9cd9761a07ed67c5ed231
Nirmit Vasani <nirmitvasani2000@gmail.com>
2024-07-18 00:22:40 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a = "nirmit";
+const b = "jay";
+const c = "heril";
+const d = "mayur";
+
+export default d;
+export {a};
+export {b};
+export {c};
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+import data,{a,b,c} from './module1.mjs';
+
+console.log(data);
+console.log(a);
+console.log(b);
+console.log(c);
